ec2d5b46e0cf93de737dece4ac0e1ce6a06e6ba3
Thiago dos Santos <34283656+thiago125@users.noreply.github.com>
2018-10-19 21:36:51 -0300

    Database orkut - Sql
    Consultas na tabela por meio do select, from, where, group by e having

--- orkut.sql
+++ orkut.sql
@@ -1,8 +1,9 @@
 ﻿create table usuario (
 	id_usuario int 		    not null,
 	nome_usuario varchar(45)    not null,
-	idade_usuario int,
-	qtd_amigos_usuario int,
+	idade_usuario int           not null,
+	qtd_amigos_usuario int      not null,
+	cidade_usuario varchar(45)  not null,
 
 	constraint pk_usuario
 	     primary key (id_usuario)
@@ -49,5 +50,89 @@ create table orkut(
 		foreign key (id_comunidade_orkut)
 		references comunidade (id_comunidade)
 );
+
+-- ANOTAÇÕES DE AULA 
+OBS - Quando o enunciado nao pede as colunas para serem exibidas, eu vou exibir a coluna com chave primária, e um outra coluna com uma descrição referente a coluna pk da tabela. 
+OBS - escrevi meu select, tem coluna e função de agregação, 
+eu jogo somente os nomes das colunas que escrevi no select no comando group by.
+
+-- QUESTÃO 1 
+
+select u.id_usuario, u.nome_usuario, c.id_comunidade, c.nome_comunidade
+from usuario as u, comunidade as c
+where c.proprietario_comunidade = u.id_usuario
+
+
+-- QUESTÃO 2 
+select c.id_comunidade, c.nome_comunidade, u.id_usuario, u.nome_usuario
+from usuario as u, comunidade as c, orkut as o
+where c.id_comunidade = o.id_comunidade_orkut
+        and u.id_usuario = o.id_usuario_orkut 
+
+
+-- QUESTÃO 3 
+SELECT u.id_usuario, u.nome_usuario, c.id_comunidade, c.nome_comunidade
+from comunidade as c, usuario as u, orkut as o 
+where u.id_usuario = c.proprietario_comunidade
+           and u.nome_usuario = 'Maria Joaquina Cruz'
+
+
+--QUESTÃO 4 
+SELECT sum (c.qtd_usuarios_comunidade)
+FROM usuario as u, comunidade as c
+where u.id_usuario = c.proprietario_comunidade
+           and u.nome_usuario = 'Maria Joaquina Cruz'
+
+
+--QUESTÃO 5 
+select u.id_usuario, u.nome_usuario, sum(c.qtd_usuarios_comunidade)
+from usuario as u, comunidade as c 
+where u.id_usuario = c.proprietario_comunidade
+group by u.id_usuario, u.nome_usuario
+
+
+--QUESTÃO 6
+select u.id_usuario, u.nome_usuario count(*)
+from comunidade as c, usuario as u
+where c.proprietario_comunidade = u.id_usuario
+group by u.id_usuario, u.nome_usuario
+
+--QUESTÃO 7 
+ select u.id_usuario, u.nome_usuario count(*)
+ from usuario as u, amigo as a
+ where u.id_usuario = a.id_usuario2
+ group by u.id_usuario, u.nome_usuario 
+
+ --QUESTÃO 8 
+select u.id_usuario, u.nome_usuario max(*)
+from usuario as u, comunidade as c
+where u.id_usuario = c.proprietario_comunidade
+group by u.id_usuario, u.nome_usuario
+
+--QUESTÃO 9 
+ select u.id_suario, u.nome_usuario 
+ from usuario as u left outer join amigo as a
+ on u.idUsuario = a.idUsuario2
+ where a.idAmigo is null
+
+           
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
